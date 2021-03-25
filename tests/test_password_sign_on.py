3e42c273884db227dcd3460912c9cc2f08ec6575
Glen Goulter <glenrg@uw.edu>
2021-03-25 09:00:15 -0700

    [AUTH-1434] Update existing automated IdP tests to meet current manual tests PWD-1 thru PWD-9

--- tests/test_password_sign_on.py
+++ tests/test_password_sign_on.py
@@ -1,5 +1,11 @@
+"""
+This set of tests is based on the manual tests here
+https://wiki.cac.washington.edu/display/SMW/Glen%27s+IdP+practice+Matrix
+PWD-1 thru PWD-9.
+"""
+
+
 from functools import partial
-from typing import Callable
 
 import pytest
 from webdriver_recorder.browser import Chrome
@@ -16,7 +22,8 @@ def browser() -> Chrome:
 @pytest.fixture
 def new_tab(browser):
     def inner():
-        browser.find_element_by_tag_name('body').send_keys(CTRL_KEY + 't')
+        browser.execute_script("window.open('');")
+        browser.switch_to.window(browser.window_handles[-1])
     return inner
 
 
@@ -50,100 +57,130 @@ def crazy_casing(netid: str) -> str:
     return ''.join(letters)
 
 
-# PWD 1–4, 6–8, 10
+def test_pwd_1_2_3(utils, browser, sp_url, sp_domain, new_tab, secrets, netid):
+    # 1
+    password = secrets.test_accounts.password
+    sp = ServiceProviderInstance.diafine8
+    with utils.using_test_sp(sp):
+        browser.get(f'{sp_url(sp)}/shibprod')
+        browser.send_inputs(netid, password)
+        browser.click(Locators.submit_button)
+        browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
+
+    # 2
+    sp = ServiceProviderInstance.diafine6
+    with utils.using_test_sp(sp):
+        new_tab()
+        with browser.autocapture_off():
+            browser.get(f'{sp_url(sp)}/shibprod')
+        browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
+    # 3 logout and close browser
+        with browser.autocapture_off():
+            browser.click_tag('a', 'Return to the main menu')
+        with browser.autocapture_off():
+            browser.wait_for_tag('h1', 'Diafine6 IdP Testing Platform')
+        browser.click_tag('a', 'Logout')
+        browser.wait_for_tag('span', 'Your UW NetID sign-in session has ended')
+        browser.close()
+
+
+def test_pwd_4(utils, browser, sp_url, secrets, netid):
+    fresh_browser = Chrome()
+    sp = ServiceProviderInstance.diafine6
+    with utils.using_test_sp(sp):
+        fresh_browser.get(f'{sp_url(sp)}/shibprod')
+        password = secrets.test_accounts.password
+        badnetid = 'non-existant-0123456789'
+        fresh_browser.send_inputs(badnetid, password)
+        fresh_browser.click(Locators.submit_button)
+        fresh_browser.wait_for_tag('p', 'Your sign-in failed.')
+        element = fresh_browser.find_element_by_id('weblogin_netid')
+        element.click()
+        element.clear()
+        badpassword = '1'
+        fresh_browser.send_inputs(netid, badpassword)
+        fresh_browser.click(Locators.submit_button)
+        fresh_browser.wait_for_tag('p', 'Your sign-in failed.')
+        fresh_browser.close()
+
+
+def test_pwd_5(utils, browser, sp_url, secrets, netid, sp_domain, new_tab):
+    fresh_browser = Chrome()
+    sp = ServiceProviderInstance.diafine6
+    with utils.using_test_sp(sp):
+        fresh_browser.get(f'{sp_url(sp)}/shibprod')
+        password = secrets.test_accounts.password
+        fresh_browser.send_inputs(netid, password)
+        fresh_browser.click(Locators.submit_button)
+        fresh_browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
+
+    sp = ServiceProviderInstance.diafine8
+    with utils.using_test_sp(sp):
+        new_tab()
+        fresh_browser.get(f'{sp_url(sp)}/shibprodforce')
+        password = secrets.test_accounts.password
+        fresh_browser.send_inputs(netid, password)
+        fresh_browser.click(Locators.submit_button)
+        fresh_browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
+        fresh_browser.close()
+
+
+def test_pwd_6(browser, netid, utils, sp_url, secrets, sp_domain):
+    fresh_browser = Chrome()
+    sp = ServiceProviderInstance.diafine6
+    with utils.using_test_sp(sp):
+        fresh_browser.get('https://idp.u.washington.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=https://diafine6'
+                          '.sandbox.iam.s.uw.edu/shibboleth&shire=https://diafine6.sandbox.iam.s.uw.edu/Shibboleth'
+                          '.sso/SAML2/POST')
+        password = secrets.test_accounts.password
+        fresh_browser.send_inputs(netid, password)
+        fresh_browser.click(Locators.submit_button)
+        fresh_browser.wait_for_tag('h1', 'Diafine6 IdP Testing Platform')
+        fresh_browser.get('https://diafine6.sandbox.iam.s.uw.edu/Shibboleth.sso/Session')
+        fresh_browser.wait_for_tag('u', 'Miscellaneous')
+        with fresh_browser.autocapture_off():
+            fresh_browser.wait_for_tag('strong', 'Session Expiration (barring inactivity):')
+    fresh_browser.close()
+
+
 @pytest.mark.parametrize('login_transform', [
-    lambda n: n,
     partial(add_suffix, '@u.washington.edu'),
     partial(add_suffix, '@washington.edu'),
     partial(add_suffix, '@uw.edu'),
-    crazy_casing,
+    partial(add_suffix, '@google.com')
 ])
-def test_new_session_std_sign_on(utils, browser, sp_url, sp_domain, new_tab, secrets, netid, login_transform):
+def test_pwd_7(browser, netid, utils, sp_url, secrets, sp_domain, login_transform):
+    fresh_browser = Chrome()
     login = login_transform(netid)
     password = secrets.test_accounts.password
     sp = ServiceProviderInstance.diafine6
     with utils.using_test_sp(sp):
-        browser.get(f'{sp_url(sp)}/shibprod')
-        browser.send_inputs(login, password)
-        browser.click(Locators.submit_button)
-        browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
-
-    sp = ServiceProviderInstance.diafine12
-    with utils.using_test_sp(sp):
-        new_tab()
-        browser.get(f'{sp_url(sp)}/shibprod')
-        browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
-
-    sp = ServiceProviderInstance.diafine6
-    with utils.using_test_sp(sp):
-        new_tab()
-        browser.get(f'{sp_url(sp)}/shibprodforce')
-        browser.send_inputs(netid, password)
-        browser.click(Locators.submit_button)
-        browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
-        browser.click(Locators.logout_button)
-        browser.wait_for_tag('span', 'your uw netid sign-in session has ended.')
+        fresh_browser.get(f'{sp_url(sp)}/shibprod')
+        fresh_browser.send_inputs(login, password)
+        fresh_browser.click(Locators.submit_button)
+        if 'google' not in login:
+            print('not google')
+            fresh_browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
+        else:
+            print('google')
+            element = fresh_browser.wait_for_tag('p', 'Please sign in with your')
+            assert ('Please sign in with your UW NetID' in element.text)
+    fresh_browser.close()
 
 
-# PWD 5
-def test_idp_initiated_sign_on(utils, browser, sp_url, sp_domain, secrets, netid):
-    sp = ServiceProviderInstance.diafine6
+def test_pwd_8_9(browser, netid, utils, sp_url, secrets, sp_domain):
+    fresh_browser = Chrome()
     password = secrets.test_accounts.password
-    url = ("https://idp.u.washington.edu/idp/profile/SAML2/Unsolicited/SSO?providerId="
-           f"{sp_url(sp)}/shibboleth&shire={sp_url(sp)}/Shibboleth.sso/SAML2/POST")
-    with utils.using_test_sp(sp):
-        browser.get(url)
-        browser.send_inputs(netid, password)
-        browser.click(Locators.submit_button)
-        browser.wait_for_tag('h1', 'diafine6 idp testing platform')
-
-
-# PWD 9, 12, 13
-@pytest.mark.parametrize('login_transform, password_transform, expected_error_message', [
-    # Adds an unsupported suffix to the user's netid
-    (partial(add_suffix, '@google.com'), lambda p: p, 'please sign in with your uw netid'),
-    # Transforms the user's netid to become one that doesn't exist
-    (partial(add_suffix, 'zzz'), lambda p: p, 'your sign-in failed'),
-    # Reverses the user's password, so that it is incorrect
-    (lambda n: n, lambda p: p[::-1], 'your sign-in failed'),
-])
-def test_password_sign_on_fails(
-        utils: WebTestUtils,
-        browser: Chrome,
-        sp_url: Callable[[ServiceProviderInstance], str],
-        secrets: TestSecrets,
-        netid: str,
-        login_transform: Callable[[str], str],
-        password_transform: Callable[[str], str],
-        expected_error_message: str):
-    """
-    :param login_transform: A function that accepts a string, and returns a string.
-    :param password_transform:  A function that accepts a string, and returns a string.
-    """
-    login = login_transform(netid)
     sp = ServiceProviderInstance.diafine6
-    password = password_transform(secrets.test_accounts.password)
-
+    crazy_netid = crazy_casing(netid)
     with utils.using_test_sp(sp):
-        browser.get(f'{sp_url(sp)}/shibprod')
-        browser.send_inputs(login, password)
-        browser.click(Locators.submit_button)
-        browser.wait_for_tag('p', expected_error_message)
-
-
-# PWD 11
-def test_query_param_forwarding(utils: WebTestUtils, browser: Chrome, sp_url: Callable[[ServiceProviderInstance], str],
-                                secrets: TestSecrets, netid: str):
-    sp = ServiceProviderInstance.diafine6
-    password = secrets.test_accounts.password
-
-    with utils.using_test_sp(sp):
-
-        browser.get(f'{sp_url(sp)}/shibprod/q-param-test.aspx?fname=Joe&lname=Smith&age=30')
-        browser.send_inputs(netid, password)
-        browser.click(Locators.submit_button)
-        browser.wait_for_tag('h1', 'query parameters')
-        paragraphs = browser.find_elements_by_tag_name('p')
-        assert 'this page should display all parameters from the query string' in paragraphs[0].text
+        fresh_browser.get(f'{sp_url(sp)}/shibprod')
+        fresh_browser.send_inputs(crazy_netid, password)
+        fresh_browser.click(Locators.submit_button)
+        fresh_browser.wait_for_tag('h2', f'{sp_domain(sp)} sign-in success!')
+        # 9
+        fresh_browser.get(f'{sp_url(sp)}/shibprod/q-param-test.aspx?fname=Joe&lname=Smith&age=30')
+        fresh_browser.wait_for_tag('h1', 'query parameters')
+        element = fresh_browser.find_elements_by_tag_name('p')
         for snippet in ('fname = Joe', 'lname = Smith', 'age = 30'):
-            assert snippet in paragraphs[1].text
+            assert (snippet in element[1].text)
