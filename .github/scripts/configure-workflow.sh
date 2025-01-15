290477c951481fce2f40ae0028d5cdfce3604adf
Glen Goulter <37945000+goulter@users.noreply.github.com>
2025-01-15 14:52:07 -0800

    fix the test result ipd env, test start and end reason

--- .github/scripts/configure-workflow.sh
+++ .github/scripts/configure-workflow.sh
@@ -112,6 +112,8 @@ function get-input-reason() {
     REASON="$INPUT_REASON"
   elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
     REASON="Triggered from PR"
+  elif [ "$GITHUB_EVENT_NAME" = "schedule" ]; then
+    REASON="Scheduled run"
   else
     REASON="No reason provided"
   fi
